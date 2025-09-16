e89e25ee421a4c547a5942682a19c6f8436c34cb
pranith-singh <thakurpranithraj@gmail.com>
2025-09-16 15:09:51 +0530

    Create todoRoutes.js

---
+++ backend/routes/todoRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+router.get('/', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+router.post('/', async (req, res) => {
+  const newTodo = new Todo(req.body);
+  const savedTodo = await newTodo.save();
+  res.json(savedTodo);
+});
+
+router.put('/:id', async (req, res) => {
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTodo);
+});
+
+router.delete('/:id', async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Todo deleted' });
+});
+
+module.exports = router;
